2165db054b63cf6c018cd270762b15839bcd5b25
Antti Lahtinen <antlaht86@gmail.com>
2018-02-19 10:54:57 +0200

    Add post / todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+
+module.exports = {Todo};
+
+
+
+// var twoTodo = new Todo({
+//     text: ' some text ' //<-- Tämä ei hyväksy objectia, mutta hyväksyy numeron ja booleanin, koska muuttaa ne stringiksi
+// });
+
+// twoTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+
+
+var User = mongoose.model('Users', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+module.exports = { User };
+
+
+
+
+
+// var newUser = new User({
+//     email: ' antlaht86@gmail.com   '
+// });
+
+// newUser.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// });
