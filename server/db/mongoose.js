ea5638af7ad9b95b5d256ffb511304a29f57f07b
Antti Lahtinen <antlaht86@gmail.com>
2018-03-05 18:49:49 +0200

    jotain

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todoApp');
-
+mongoose.connect( 'mongodb://antlaht:KolmeKaksi32@ds251988.mlab.com:51988/todoapp'||'mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
